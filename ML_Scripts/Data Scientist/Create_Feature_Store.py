d45ce35c06c8dd3895eb5a09c2ef1b3e05c8a299
ciaran28 <ciaran28>
2022-11-05 15:57:27 +0000

    Feature Engineering Demo

--- ML_Scripts/Data Scientist/Create_Feature_Store.py
+++ ML_Scripts/Data Scientist/Create_Feature_Store.py
@@ -1,2 +1,18 @@
 # Databricks notebook source
+# https://www.advancinganalytics.co.uk/blog/2022/2/17/databricks-feature-store-tutorial
 
+import numpy as np
+import pandas as pd 
+from pyspark.sql.functions import *
+from databricks import feature_store
+
+# COMMAND ----------
+
+#Alter this path to point to the location of where you have uploaded your train.csv
+dbfs_path = 'dbfs:/FileStore/tables/train.csv'
+df_train = spark.read.csv(dbfs_path, header = "True", inferSchema="True")
+
+# COMMAND ----------
+
+# Display training data
+display(df_train)
