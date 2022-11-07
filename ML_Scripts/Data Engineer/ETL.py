4608c561a266e3d0f93bec58e5efbe60b0f0eec5
ciaran28 <ciaran28>
2022-11-07 00:30:25 +0000

    Test

--- ML_Scripts/Data Engineer/ETL.py
+++ ML_Scripts/Data Engineer/ETL.py
@@ -1,12 +1,16 @@
 # Databricks notebook source
 # Take Data and Transform it, So that it may be used in the Feature Engineering stage
-
 # Raw Data Container --> ETL --> Feature Data Container 
 
 import numpy as np
 import pandas as pd 
 from pyspark.sql.functions import *
 from databricks import feature_store
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Authenticate To Service Principal
 
 # COMMAND ----------
 
@@ -23,85 +27,118 @@ DBX_SP_TenantID = dbutils.secrets.get(scope="DBX_SP_Credentials",key="DBX_SP_Ten
 # Ensure that RABC Blob contributor is set onto the container for the SP????
 
 
-spark.conf.set("fs.azure.account.auth.type.testsachd.dfs.core.windows.net", "OAuth")
-spark.conf.set("fs.azure.account.oauth.provider.type.testsachd.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-spark.conf.set("fs.azure.account.oauth2.client.id.testsachd.dfs.core.windows.net", "7e83398e-8440-4d0c-8533-44c8b90bfa83")
-spark.conf.set("fs.azure.account.oauth2.client.secret.testsachd.dfs.core.windows.net", "kCq8Q~P2Do0CET~h8zauDNKMULVZ3uuZu~bsXah8")   
-spark.conf.set("fs.azure.account.oauth2.client.endpoint.testsachd.dfs.core.windows.net", "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token")
-spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
-display(dbutils.fs.ls("abfss://new@testsachd.dfs.core.windows.net/hvkgv"))
-spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
+#spark.conf.set("fs.azure.account.auth.type.testsachd.dfs.core.windows.net", "OAuth")
+#spark.conf.set("fs.azure.account.oauth.provider.type.testsachd.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+#spark.conf.set("fs.azure.account.oauth2.client.id.testsachd.dfs.core.windows.net", "7e83398e-8440-4d0c-8533-44c8b90bfa83")
+#spark.conf.set("fs.azure.account.oauth2.client.secret.testsachd.dfs.core.windows.net", "kCq8Q~P2Do0CET~h8zauDNKMULVZ3uuZu~bsXah8")   
+#spark.conf.set("fs.azure.account.oauth2.client.endpoint.testsachd.dfs.core.windows.net", "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token")
+#spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
+#display(dbutils.fs.ls("abfss://new@testsachd.dfs.core.windows.net/hvkgv"))
+#spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
 
 
 spark.conf.set("fs.azure.account.auth.type.adlsdevgayt.dfs.core.windows.net", "OAuth")
 spark.conf.set("fs.azure.account.oauth.provider.type.adlsdevgayt.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-spark.conf.set("fs.azure.account.oauth2.client.id.adlsdevgayt.dfs.core.windows.net", "7e83398e-8440-4d0c-8533-44c8b90bfa83")
-spark.conf.set("fs.azure.account.oauth2.client.secret.adlsdevgayt.dfs.core.windows.net", "kCq8Q~P2Do0CET~h8zauDNKMULVZ3uuZu~bsXah8")   
+spark.conf.set("fs.azure.account.oauth2.client.id.adlsdevgayt.dfs.core.windows.net", dbutils.secrets.get(scope="DBX_SP_Credentials",key="DBX_SP_ClientID") )
+spark.conf.set("fs.azure.account.oauth2.client.secret.adlsdevgayt.dfs.core.windows.net", dbutils.secrets.get(scope="DBX_SP_Credentials",key="DBX_SP_Client_Secret") )   
 spark.conf.set("fs.azure.account.oauth2.client.endpoint.adlsdevgayt.dfs.core.windows.net", "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token")
 spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
 display(dbutils.fs.ls("abfss://raw@adlsdevgayt.dfs.core.windows.net"))
 spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
 
+#spark.conf.set("fs.azure.account.auth.type.adlsdevgayt.dfs.core.windows.net", "OAuth")
+#spark.conf.set("fs.azure.account.oauth.provider.type.adlsdevgayt.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+#spark.conf.set("fs.azure.account.oauth2.client.id.adlsdevgayt.dfs.core.windows.net", "7e83398e-8440-4d0c-8533-44c8b90bfa83")
+#spark.conf.set("fs.azure.account.oauth2.client.secret.adlsdevgayt.dfs.core.windows.net", "kCq8Q~P2Do0CET~h8zauDNKMULVZ3uuZu~bsXah8")   
+#spark.conf.set("fs.azure.account.oauth2.client.endpoint.adlsdevgayt.dfs.core.windows.net", "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token")
+#spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
+#display(dbutils.fs.ls("abfss://raw@adlsdevgayt.dfs.core.windows.net"))
+#spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
 
 
+configs = {"fs.azure.account.auth.type": "OAuth",
+           "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+           "fs.azure.account.oauth2.client.id": "7e83398e-8440-4d0c-8533-44c8b90bfa83",
+           "fs.azure.account.oauth2.client.secret": "kCq8Q~P2Do0CET~h8zauDNKMULVZ3uuZu~bsXah8",
+           "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token"}
+
+#display(dbutils.fs.ls("abfss://raw@adlsdevgayt.dfs.core.windows.net"))
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Ingest CSV From Data Lake & Display
+
+# COMMAND ----------
 
 dbfs_path = 'abfss://raw@adlsdevgayt.dfs.core.windows.net/train.csv'
 df_train = spark.read.csv(dbfs_path, header = "True", inferSchema="True")
 
 df_train.display()
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Establish Mount Point
+
+# COMMAND ----------
+
+
+mount_point = "/mnt/titanicdata"
+
+if all(mount.mountPoint != mount_point for mount in dbutils.fs.mounts()):
+    dbutils.fs.mount(source = "abfss://raw@adlsdevgayt.dfs.core.windows.net/training_data", mount_point = mount_point, extra_configs = configs)
+        
 
 
 # COMMAND ----------
 
-dbutils.fs.mount(
-source = "abfss://raw@adlsdevgayt.dfs.core.windows.net/training_data",
-mount_point = "/mnt/flightdata",
-extra_configs = configs)
-
-# COMMAND ----------
-
+#dbutils.fs.unmount("/mnt/titanicdata")
 display(dbutils.fs.mounts())
 
-# COMMAND ----------
 
-df = spark.read.format("csv").load("/mnt/flightdata/train.csv")
-df.display()
 
 # COMMAND ----------
 
-DBX_SP_Client_Secret = dbutils.secrets.get(scope="DBX_SP_Credentials",key="DBX_SP_Client_Secret")
-DBX_SP_ClientID = dbutils.secrets.get(scope="DBX_SP_Credentials",key="DBX_SP_ClientID")
-DBX_SP_TenantID = dbutils.secrets.get(scope="DBX_SP_Credentials",key="DBX_SP_TenantID")
-
-print(DBX_SP_Client_Secret)
-print(DBX_SP_ClientID)
-print(DBX_SP_TenantID)
-
-spark.conf.set("fs.azure.account.auth.type.adlsdevgayt.dfs.core.windows.net", "OAuth")
-spark.conf.set("fs.azure.account.oauth.provider.type.adlsdevgayt.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-spark.conf.set("fs.azure.account.oauth2.client.id.adlsdevgayt.dfs.core.windows.net", DBX_SP_ClientID)
-spark.conf.set("fs.azure.account.oauth2.client.secret.adlsdevgayt.dfs.core.windows.net", DBX_SP_Client_Secret)
-spark.conf.set("fs.azure.account.oauth2.client.endpoint.adlsdevgayt.dfs.core.windows.net", "https://login.microsoftonline.com/"+DBX_SP_TenantID+"/oauth2/token")
-
-
-
-
-
-dbfs_path = 'dbfs:/FileStore/tables/train.csv'
-df_train = spark.read.csv(dbfs_path, header = "True", inferSchema="True")
+# MAGIC %md
+# MAGIC # Read in CSV as Data Frame From Mount Point
 
 # COMMAND ----------
 
-
-display(df_train)
-
-# COMMAND ----------
-
-# Display training data
-2
-display(df_train)
+df_train = spark.read.csv(mount_point, header = "True", inferSchema="True")
+df_train.display()
 
 # COMMAND ----------
 
 df_train.printSchema()
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC # Cleaning Data
+
+# COMMAND ----------
+
+df_train = (df_train 
+                 .withColumnRenamed("Pclass", "PassengerClass") 
+                 .withColumnRenamed("SibSp", "SiblingsSpouses") 
+                 .withColumnRenamed("Parch", "ParentsChildren"))
+df_train.display()
+
+# COMMAND ----------
+
+df_train.display()
+
+# Add a new Feature column "Title"
+df = df_train.withColumn("Title",regexp_extract(col("Name"),"([A-Za-z]+)\.",1))
+df.display()
+
+# Sanitise and group titles
+# 'Mlle', 'Mme', 'Ms' --> Miss
+# 'Lady', 'Dona', 'Countess' --> Mrs
+# 'Dr', 'Master', 'Major', 'Capt', 'Sir', 'Don' --> Mr
+# 'Jonkheer' , 'Col' , 'Rev' --> Other
+df = df.replace(['Mlle','Mme', 'Ms', 'Dr','Master','Major','Lady','Dona','Countess','Jonkheer','Col','Rev','Capt','Sir','Don'],
+                ['Miss','Miss','Miss','Mr','Mr', 'Mr', 'Mrs',  'Mrs', 'Mrs', 'Other',  'Other','Other','Mr','Mr','Mr'])
+
