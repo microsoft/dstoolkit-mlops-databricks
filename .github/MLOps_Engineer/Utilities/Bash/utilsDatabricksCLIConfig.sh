b3798ef419c6747bbac3d61c33168b781803f8b6
ciaran28 <ciaranh@microsoft.com>
2022-12-04 22:21:46 +0000

    Configure PAT Token Creation

--- .github/MLOps_Engineer/Utilities/Bash/utilsDatabricksCLIConfig.sh
+++ .github/MLOps_Engineer/Utilities/Bash/utilsDatabricksCLIConfig.sh
@@ -2,16 +2,8 @@
 pip3 install databricks-cli --upgrade
 
 
-echo "Ingest JSON File"
-JSON=$( jq '.' MLOps_Engineer/2-Infrastructure_Layer/DBX_CICD_Deployment/Bicep_Params/$Environment/Bicep.parameters.json)
-resourceGroupName=$( jq -r '.parameters.resourceGroupName.value' <<< "$JSON")
-echo "Resource Group Name: $resourceGroupName"
-
-AZ_KEYVAULT_NAME=$(az keyvault list -g $resourceGroupName --query "[].name" -o tsv)
+AZ_KEYVAULT_NAME=$(az keyvault list -g $RESOURCE_GROUP_NAME --query "[].name" -o tsv)
 DATABRICKS_TOKEN=$(az keyvault secret show --name "dbkstoken" --vault-name $AZ_KEYVAULT_NAME --query "value" -o tsv)
-
-echo "Set Databricks Token As Environment Variable..."
-echo "##vso[task.setvariable variable="DATABRICKS_TOKEN";isOutput=true;]$DATABRICKS_TOKEN"
 
 echo $DATABRICKS_HOST
 echo $DATABRICKS_TOKEN
