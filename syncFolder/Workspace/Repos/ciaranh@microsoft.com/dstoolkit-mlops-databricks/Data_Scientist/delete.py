b13031b2a95e357b090cdf7766d3d4f0f8ebe229
ciaran28 <ciaranh@microsoft.com>
2022-12-10 16:13:13 +0000

    Dev Testing

---
+++ syncFolder/Workspace/Repos/ciaranh@microsoft.com/dstoolkit-mlops-databricks/Data_Scientist/delete.py
@@ -0,0 +1,22 @@
+# experiments2
+
+arrayTest = [[1,2,3],[4,5,6],[7,8,9]] 
+
+for element in arrayTest:
+    print(tuple(element[2]))
+
+#[tuple(element[2][0][0])[0] for element in arrayTest]
+results = []
+
+
+
+lhs = []
+rhs = []
+for result in results:
+    lhsTuple = tuple(result[2][0][0])[0]
+    lhs.append(lhsTuple)
+
+    rhsTuple = tuple(result[2][0][1])[0]
+    rhs.append(lhsTuple)
+
+
