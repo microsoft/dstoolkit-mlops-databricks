70492f0c0107ca096a4275453779e2b1225babab
ciaran28 <ciaranh@microsoft.com>
2023-01-31 14:09:45 +0000

    KV Environment Location From UK to US

--- .github/MLOps_Engineer/Utilities/Bash/utilsSetEnvVariables.sh
+++ .github/MLOps_Engineer/Utilities/Bash/utilsSetEnvVariables.sh
@@ -16,6 +16,7 @@ DATABRICKS_INSTANCE="$(az databricks workspace list -g $RESOURCE_GROUP_NAME --qu
 WORKSPACE_ID=$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query "[].id" -o tsv)
 AZ_KEYVAULT_NAME=$(az keyvault list -g $RESOURCE_GROUP_NAME --query "[].name" -o tsv)
 SUBSCRIPTION_ID=$( az account show --query id -o tsv )
+echo $SUBSCRIPTION_ID
 #DATABRICKS_TOKEN=$(az keyvault secret show --name "dbkstoken" --vault-name $AZ_KEYVAULT_NAME --query "value" -o tsv)
 
 
