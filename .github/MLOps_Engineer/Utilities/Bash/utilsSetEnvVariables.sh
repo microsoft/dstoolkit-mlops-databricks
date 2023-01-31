940636aa0855dcab461df18d4dff7229422aa3aa
ciaran28 <ciaranh@microsoft.com>
2023-01-31 14:20:32 +0000

    Bug Fix

--- .github/MLOps_Engineer/Utilities/Bash/utilsSetEnvVariables.sh
+++ .github/MLOps_Engineer/Utilities/Bash/utilsSetEnvVariables.sh
@@ -15,7 +15,7 @@ DATABRICKS_ORDGID=$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query
 DATABRICKS_INSTANCE="$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query "[].workspaceUrl" -o tsv)"
 WORKSPACE_ID=$(az databricks workspace list -g $RESOURCE_GROUP_NAME --query "[].id" -o tsv)
 AZ_KEYVAULT_NAME=$(az keyvault list -g $RESOURCE_GROUP_NAME --query "[].name" -o tsv)
-SUBSCRIPTION_ID=$( az account show --query id  )
+SUBSCRIPTION_ID=$( az account show --query id -o tsv )
 echo $SUBSCRIPTION_ID
 echo $DATABRICKS_ORDGID
 echo $WORKSPACE_ID
