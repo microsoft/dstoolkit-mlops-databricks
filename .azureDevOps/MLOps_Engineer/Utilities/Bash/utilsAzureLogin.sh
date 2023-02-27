08c0d98f742c97cea1d3f69dc0d89e37b2c79852
ciaran28 <ciaranh@microsoft.com>
2023-02-27 11:35:47 +0000

    Configuring Managed Identity

--- .azureDevOps/MLOps_Engineer/Utilities/Bash/utilsAzureLogin.sh
+++ .azureDevOps/MLOps_Engineer/Utilities/Bash/utilsAzureLogin.sh
@@ -4,9 +4,20 @@ az upgrade
 echo $ARM_CLIENT_ID
 echo $ARM_CLIENT_SECRET
 echo $ARM_TENANT_ID
+echo $AuthenticationType
 
 az config set extension.use_dynamic_install=yes_without_prompt
 
-az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 
-az account list
+if [ $AuthenticationType == 'Managed_Identity' ]; then
+
+    echo "Managed Identity Authentication (Through Virtual Machine Scale Set)"
+    az login --identity
+    az account list
+
+else
+    echo "Service Principal Authentication"
+    az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+    az account list
+fi
+
