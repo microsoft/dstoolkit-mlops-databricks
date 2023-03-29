9780a56f5ec40894a33b274ccdebe6975dc5ae67
ciaran28 <ciaranh@microsoft.com>
2023-03-29 06:14:46 +0100

    refactoring

--- Data_Scientist/featureEngineering.py
+++ Data_Scientist/featureEngineering.py
@@ -21,6 +21,11 @@ from helperFunctions.helperFunction import *
 
 # COMMAND ----------
 
+
+experiment_id = dbutils.widgets.get("--AZUREML_EXPERIMENT_ID")
+print(experiment_id)
+# COMMAND ----------
+
 # Ingest Parameters Files
 
 
