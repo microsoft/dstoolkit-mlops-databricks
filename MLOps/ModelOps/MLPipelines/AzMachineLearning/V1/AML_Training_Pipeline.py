ae243988602d5c31badd82e4e071ec07d992ff66
ciaran28 <ciaranh@microsoft.com>
2023-04-19 10:36:52 +0100

    Development

--- MLOps/ModelOps/MLPipelines/AzMachineLearning/V1/AML_Training_Pipeline.py
+++ MLOps/ModelOps/MLPipelines/AzMachineLearning/V1/AML_Training_Pipeline.py
@@ -6,17 +6,29 @@ from azureml.pipeline.steps import PythonScriptStep, DatabricksStep
 from azureml.pipeline.core import Pipeline, PipelineData, StepSequence
 from azureml.core.compute import ComputeTarget, DatabricksCompute
 from azureml.exceptions import ComputeTargetException
+from azureml.core.authentication import ServicePrincipalAuthentication
 
 
 DATABRICKS_COMPUTE_NAME = os.environ['DATABRICKS_COMPUTE_NAME']
 RESOURCE_GROUP_NAME = os.environ['RESOURCE_GROUP_NAME']
 DATABRICKS_TOKEN = os.environ['DATABRICKS_TOKEN']
 DATABRICKS_INSTANCE = os.environ['DATABRICKS_INSTANCE']
+WORKSPACE_ID = os.environ['WORKSPACE_ID']
 SUBSCRIPTION_ID = os.environ['SUBSCRIPTION_ID']
 ARM_CLIENT_SECRET = os.environ['ARM_CLIENT_SECRET']
 ARM_TENANT_ID = os.environ['ARM_TENANT_ID']
 ARM_CLIENT_ID = os.environ['ARM_CLIENT_ID']
 DBRKS_CLUSTER_ID = os.environ['DBRKS_CLUSTER_ID']
+DBRKS_BEARER_TOKEN = os.environ['DBRKS_BEARER_TOKEN']
+DBRKS_MANAGEMENT_TOKEN = os.environ['DBRKS_MANAGEMENT_TOKEN']
+ENVIRONMENT = os.environ['ENVIRONMENT']
+
+DBRKS_REQ_HEADERS = {
+    'Authorization': f'Bearer {DBRKS_BEARER_TOKEN}',
+    'X-Databricks-Azure-SP-Management-Token': f'{DBRKS_MANAGEMENT_TOKEN}',
+    'X-Databricks-Azure-Workspace-Resource-Id': f'{WORKSPACE_ID}',
+    'Content-Type': 'application/json'
+}
 
 print(DATABRICKS_COMPUTE_NAME)
 print(RESOURCE_GROUP_NAME)
@@ -40,7 +52,7 @@ def listClusters():
     else:
         return response.json()
 
-def create_pipeline_structure(compute_target: ComputeTarget, workspace: Workspace):
+def create_pipeline_structure(compute_target: ComputeTarget, workspace: Workspace, cluster_id):
     print('Creating the pipeline structure')
 
     Databricks_Featurization_Step = DatabricksStep(
@@ -50,7 +62,7 @@ def create_pipeline_structure(compute_target: ComputeTarget, workspace: Workspac
         #    'myparam2': pipeline_param},
         run_name='Databricks_Feature_Engineering',
         compute_target=databricks_compute,
-        existing_cluster_id=DBRKS_CLUSTER_ID,
+        existing_cluster_id=cluster_id,
         allow_reuse=True
     )
 
@@ -62,7 +74,7 @@ def create_pipeline_structure(compute_target: ComputeTarget, workspace: Workspac
         #    'myparam2': pipeline_param},
         run_name='Databricks_Model_Training',
         compute_target=databricks_compute,
-        existing_cluster_id=DBRKS_CLUSTER_ID,
+        existing_cluster_id=cluster_id,
         allow_reuse=True
     )
 
@@ -73,52 +85,58 @@ def create_pipeline_structure(compute_target: ComputeTarget, workspace: Workspac
 
     return pipeline
 
-import os
-from azureml.core.authentication import ServicePrincipalAuthentication
 
-svc_pr = ServicePrincipalAuthentication(
-                        tenant_id = ARM_TENANT_ID,
-                        service_principal_id = ARM_CLIENT_ID,
-                        service_principal_password = ARM_CLIENT_SECRET 
-                        )
+if __name__ == "__main__":
 
-ws = Workspace(
-        subscription_id=SUBSCRIPTION_ID,
-        resource_group=RESOURCE_GROUP_NAME,
-        workspace_name=DATABRICKS_INSTANCE,
-        auth=svc_pr
-        )
+    svc_pr = ServicePrincipalAuthentication(
+                            tenant_id = ARM_TENANT_ID,
+                            service_principal_id = ARM_CLIENT_ID,
+                            service_principal_password = ARM_CLIENT_SECRET 
+                            )
 
-print(f" AML Workspace Properties: {ws} ")
+    ws = Workspace(
+            subscription_id=SUBSCRIPTION_ID,
+            resource_group=RESOURCE_GROUP_NAME,
+            workspace_name=DATABRICKS_INSTANCE,
+            auth=svc_pr
+            )
 
-try:
-    DATABRICKS_COMPUTE_NAME = DatabricksCompute(workspace=ws, name=DATABRICKS_COMPUTE_NAME)
-    print('Compute target {} already exists'.format(DATABRICKS_COMPUTE_NAME))
+    print(f" AML Workspace Properties: {ws} ")
 
-except ComputeTargetException:
-    print('Compute not found, will use below parameters to attach new one')
-    print('db_compute_name {}'.format(DATABRICKS_COMPUTE_NAME))
-    print('db_resource_group {}'.format(RESOURCE_GROUP_NAME))
-    print('db_workspace_name {}'.format(DATABRICKS_INSTANCE))
-    print('db_access_token {}'.format(DATABRICKS_TOKEN))
+    try:
+        DATABRICKS_COMPUTE_NAME = DatabricksCompute(workspace=ws, name=DATABRICKS_COMPUTE_NAME)
+        print('Compute target {} already exists'.format(DATABRICKS_COMPUTE_NAME))
 
-    config = DatabricksCompute.attach_configuration(
-        resource_group = RESOURCE_GROUP_NAME,
-        workspace_name = DATABRICKS_INSTANCE,
-        access_token= DATABRICKS_TOKEN)
-    databricks_compute=ComputeTarget.attach(ws, DATABRICKS_COMPUTE_NAME, config)
-    databricks_compute.wait_for_completion(True)
+    except ComputeTargetException:
+        print('Compute not found, will use below parameters to attach new one')
+        print('db_compute_name {}'.format(DATABRICKS_COMPUTE_NAME))
+        print('db_resource_group {}'.format(RESOURCE_GROUP_NAME))
+        print('db_workspace_name {}'.format(DATABRICKS_INSTANCE))
+        print('db_access_token {}'.format(DATABRICKS_TOKEN))
 
-#
+        config = DatabricksCompute.attach_configuration(
+            resource_group = RESOURCE_GROUP_NAME,
+            workspace_name = DATABRICKS_INSTANCE,
+            access_token= DATABRICKS_TOKEN)
+        databricks_compute=ComputeTarget.attach(ws, DATABRICKS_COMPUTE_NAME, config)
+        databricks_compute.wait_for_completion(True)
 
-existingClusters = listClusters()
+    
+    existingClusters = listClusters()['clusters']
+    for cluster in existingClusters:
+        if cluster['cluster_name'] == "ml_cluster":
+            print("ml_cluster exists")
+            cluster_id = cluster['cluster_id']
+            print(cluster_id)
+        else:
+            print("ml_cluster does not exist: cannot continue")
 
 
-#notebook_path=os.getenv("DATABRICKS_NOTEBOOK_PATH", "/Data_Scientist/featureEngineering.py")
-notebook_path=os.getenv("DATABRICKS_NOTEBOOK_PATH", "databricks.ipynb")
+    #notebook_path=os.getenv("DATABRICKS_NOTEBOOK_PATH", "/Data_Scientist/featureEngineering.py")
+    notebook_path=os.getenv("DATABRICKS_NOTEBOOK_PATH", "databricks.ipynb")
 
-pipeline = create_pipeline_structure(compute_target=databricks_compute,  workspace=ws)
+    pipeline = create_pipeline_structure(compute_target=databricks_compute,  workspace=ws, cluster_id=cluster_id)
 
-published_pipeline = pipeline.publish("databricks_pipeline", version="1.0.0", description="Databricks Pipeline")
+    published_pipeline = pipeline.publish("databricks_pipeline", version="1.0.0", description="Databricks Pipeline")
 
 
